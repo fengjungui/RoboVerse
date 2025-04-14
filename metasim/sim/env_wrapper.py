2b02e0d4fd746e543c4d43a86ea4b84e0a252b95
Feishi Wang <feishiwang2001@gmail.com>
2025-04-13 21:13:26 -0700

    env_wrapper simulate() after set_states() to help mujoco and isaacgym success for stack_cube (and other simuliar tasks that depends on checker initialization)

--- metasim/sim/env_wrapper.py
+++ metasim/sim/env_wrapper.py
@@ -75,6 +75,9 @@ def GymEnvWrapper(cls: type[THandler]) -> type[EnvWrapper[THandler]]:
             self._episode_length_buf[env_ids] = 0
             if states is not None:
                 self.handler.set_states(states, env_ids=env_ids)
+            if self.handler.scenario.sim in ["mujoco", "isaacgym"]:
+                ## HACK
+                self.handler.simulate()
             self.handler.checker.reset(self.handler, env_ids=env_ids)
             self.handler.refresh_render()
             states = self.handler.get_states()
